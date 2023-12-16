762d486f3d3f46f479dab343641ba5f1a6f9d0ff
KeerthanaC38 <keerthananakka38@gmail.com>
2023-12-16 13:25:21 -0600

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/** ===================== typing animmation =================== */
+var typed = new Typed(".typing",{
+    strings:["","Database Developer", "Backend Engineer","Web Developer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
